9d8b95d6f6f2603ac6bbf2866c5c9b47a42a1d99
Ryan Hilfers <rhilfers@expedia.com>
2019-02-27 19:13:42 -0800

    refactored redundant headers out of http_recorders

--- haystack/http_recorder.py
+++ haystack/http_recorder.py
@@ -10,11 +10,18 @@ logger = logging.getLogger(__name__)
 DEFAULT_TIMEOUT = 5.0
 
 
+def response_hook(response, *args, **kwargs):
+    if response.status_code in range(200, 203):
+        logger.debug("successfully submitted the span to http collector")
+    else:
+        logger.error(f"Failed to submit span to the http collector. Haystack Response: {response}")
+
+
 class SyncHttpRecorder(SpanRecorder):
     """Http span recorder which Translates and reports haystack.Spans in-process."""
 
     def __init__(self, collector_url="http://haystack-collector:8080/span", headers={},
-                 timeout_seconds=DEFAULT_TIMEOUT, use_json_payload=False):
+                 timeout_seconds=DEFAULT_TIMEOUT, use_json_payload=False, requests_session=None):
         """
         :param collector_url: the haystack collector endpoint
         :param timeout_seconds: timeout limit of the requests
@@ -23,9 +30,10 @@ class SyncHttpRecorder(SpanRecorder):
         self._collector_url = collector_url
         self._timeout_seconds = timeout_seconds
         self._use_json_payload = use_json_payload
-        self._session = Session()
+        self._session = requests_session or Session()
         headers["Content-Type"] = "application/json" if use_json_payload else "application/octet-stream"
         self._session.headers.update(headers)
+        self._session.hooks["response"] = response_hook
 
     @staticmethod
     def get_json_payload(span):
@@ -50,7 +58,7 @@ class SyncHttpRecorder(SpanRecorder):
         self.post_payload(payload)
 
 
-class ExceptionAwareRequestsSession(Session):
+class ExceptionHandlingRequestsSession(Session):
     """This class is needed to prevent exceptions from being swallowed due to the async recorder not calling .result()
     on the future."""
     def send(self, request, **kwargs):
@@ -58,13 +66,6 @@ class ExceptionAwareRequestsSession(Session):
             super().send(request, **kwargs)
         except RequestException as e:
             logger.error(f"Failed to submit span to the http collector due to {e}")
-
-
-def async_response_hook(response, *args, **kwargs):
-    if response.status_code in range(200, 203):
-        logger.debug("successfully submitted the span to http collector")
-    else:
-        logger.error(f"Failed to submit span to the http collector. Haystack Response: {response}")
 
 
 class AsyncHttpRecorder(SyncHttpRecorder):
@@ -82,9 +83,6 @@ class AsyncHttpRecorder(SyncHttpRecorder):
         Default is a ThreadExecutorPool with max 8 threads
         """
         super().__init__(collector_url=collector_url, headers=headers, timeout_seconds=timeout_seconds,
-                         use_json_payload=use_json_payload)
-        session = ExceptionAwareRequestsSession()
-        headers["Content-Type"] = "application/json" if use_json_payload else "application/octet-stream"
-        session.headers.update(headers)
-        session.hooks["response"] = async_response_hook
-        self._session = FuturesSession(executor=executor, session=session)
+                         use_json_payload=use_json_payload, requests_session=ExceptionHandlingRequestsSession())
+
+        self._session = FuturesSession(executor=executor, session=self._session)
