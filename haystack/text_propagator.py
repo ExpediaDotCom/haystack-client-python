170899aa72faca30fa69e56e86e0f43f69e0b68f
Ryan Hilfers <rhilfers@expedia.com>
2019-03-06 17:03:09 -0800

    can enable shared spans if desired

--- haystack/text_propagator.py
+++ haystack/text_propagator.py
@@ -2,11 +2,10 @@ from .propagator import Propagator
 from .span import SpanContext
 from opentracing import SpanContextCorruptedException
 from .constants import (
-    SPAN_ID_HEADER,
+    SPAN_ID_HEADERS,
     TRACE_ID_HEADER,
-    PARENT_SPAN_ID_HEADER,
+    PARENT_SPAN_ID_HEADERS,
     BAGGAGE_HEADER_PREFIX,
-    ALTERNATE_SPAN_ID_HEADERS,
 )
 
 
@@ -15,8 +14,8 @@ class TextPropagator(Propagator):
 
     def inject(self, span_context, carrier):
         carrier[TRACE_ID_HEADER] = span_context.trace_id
-        carrier[SPAN_ID_HEADER] = span_context.span_id
-        carrier[PARENT_SPAN_ID_HEADER] = span_context.parent_id
+        carrier[SPAN_ID_HEADERS[0]] = span_context.span_id
+        carrier[PARENT_SPAN_ID_HEADERS[0]] = span_context.parent_id
         if span_context.baggage is not None:
             for item in span_context.baggage:
                 carrier[BAGGAGE_HEADER_PREFIX + item] = \
@@ -25,10 +24,11 @@ class TextPropagator(Propagator):
     def extract(self, carrier):
         count = 0
         baggage = {}
+        parent_id = None
 
         for key in carrier:
             value = carrier[key]
-            if key == SPAN_ID_HEADER:
+            if key in SPAN_ID_HEADERS:
                 span_id = value
                 count += 1
             elif key == TRACE_ID_HEADER:
@@ -36,13 +36,16 @@ class TextPropagator(Propagator):
                 count += 1
             elif key.startswith(BAGGAGE_HEADER_PREFIX):
                 baggage[key[len(BAGGAGE_HEADER_PREFIX):]] = value
-            elif key in ALTERNATE_SPAN_ID_HEADERS:
-                span_id = value
-                count += 1
+            elif key in PARENT_SPAN_ID_HEADERS:
+                parent_id = value
 
         if count == 0:
             return None
         elif count != 2:
-            raise SpanContextCorruptedException()
+            raise SpanContextCorruptedException(
+                "Both SpanID and TraceID are required")
 
-        return SpanContext(span_id=span_id, trace_id=trace_id, baggage=baggage)
+        return SpanContext(span_id=span_id,
+                           trace_id=trace_id,
+                           parent_id=parent_id,
+                           baggage=baggage)
--- tests/unit/test_tracer.py
+++ tests/unit/test_tracer.py
@@ -1,6 +1,7 @@
 import unittest
 from haystack import HaystackTracer
 from haystack.recorder import NoopRecorder
+from haystack.span import SpanContext
 
 
 class HaystackTracerTests(unittest.TestCase):
@@ -11,7 +12,8 @@ class HaystackTracerTests(unittest.TestCase):
 
     def test_common_tags_are_applied_to_new_spans(self):
         common_tags = {"a": "common_tag", "another": "common_tag"}
-        tracer = HaystackTracer("any_service", NoopRecorder(), common_tags=common_tags)
+        tracer = HaystackTracer("any_service", NoopRecorder(),
+                                common_tags=common_tags)
 
         span = tracer.start_span("any_operation")
 
@@ -19,13 +21,43 @@ class HaystackTracerTests(unittest.TestCase):
 
     def test_common_tags_are_overridden_if_span_specifies_the_same(self):
         common_tags = {"a": "common_tag", "another": "common_tag"}
-        tracer = HaystackTracer("any_service", NoopRecorder(), common_tags=common_tags)
+        tracer = HaystackTracer("any_service", NoopRecorder(),
+                                common_tags=common_tags)
 
         span_tags = {"another": "span_tag"}
         span = tracer.start_span("any_operation", tags=span_tags)
 
         expected_tags = {**common_tags, **span_tags}
         self.assertDictEqual(expected_tags, span.tags)
+
+    def test_non_shared_spans_are_created_by_default(self):
+        tracer = HaystackTracer("any_service", NoopRecorder())
+        trace_id = "123"
+        span_id = "1234"
+        parent_id = "12345"
+        upstream_ctx = SpanContext(trace_id=trace_id, span_id=span_id,
+                                   parent_id=parent_id)
+
+        span = tracer.start_span("any_operation", child_of=upstream_ctx)
+
+        self.assertEqual(span.context.trace_id, trace_id)
+        self.assertNotEqual(span.context.span_id, span_id)
+        self.assertEqual(span.context.parent_id, span_id)
+
+    def test_shared_spans_are_created_when_enabled(self):
+        tracer = HaystackTracer("any_service", NoopRecorder(),
+                                use_shared_spans=True)
+        trace_id = "123"
+        span_id = "1234"
+        parent_id = "12345"
+        upstream_ctx = SpanContext(trace_id=trace_id, span_id=span_id,
+                                   parent_id=parent_id)
+
+        span = tracer.start_span("any_operation", child_of=upstream_ctx)
+
+        self.assertEqual(span.context.trace_id, trace_id)
+        self.assertEqual(span.context.span_id, span_id)
+        self.assertEqual(span.context.parent_id, parent_id)
 
 
 if __name__ == "__main__":
