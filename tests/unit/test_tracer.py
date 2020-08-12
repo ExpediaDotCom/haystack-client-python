44e09a1b23aaf6d5eb6518207fbd59c064c6616f
mmenindezsegura <mmenindezsegura@expediagroup.com>
2020-08-12 16:08:15 -0700

    Enhancing Tracer to use B3 style ids via flag

--- tests/unit/test_tracer.py
+++ tests/unit/test_tracer.py
@@ -59,6 +59,22 @@ class HaystackTracerTests(unittest.TestCase):
         self.assertEqual(span.context.span_id, span_id)
         self.assertEqual(span.context.parent_id, parent_id)
 
+    def test_b3_format_ids_are_used_when_enabled(self):
+        tracer = HaystackTracer("any_service", NoopRecorder, use_b3_ids=True)
+        span = tracer.start_span("any_operation")
+
+        self.assertTrue("-" not in span.context.trace_id)
+        self.assertTrue(len(span.context.trace_id) == 32)
+        self.assertTrue("-" not in span.context.span_id)
+        self.assertTrue(len(span.context.span_id) == 32)
+
+    def test_b3_format_ids_are_not_used_when_disabled(self):
+        tracer = HaystackTracer("any_service", NoopRecorder, use_b3_ids=False)
+        span = tracer.start_span("any_operation")
+
+        self.assertTrue("-" in span.context.trace_id)
+        self.assertTrue("-" in span.context.span_id)
+
 
 if __name__ == "__main__":
     unittest.main()
